468ccc12b61ec2ac6f7f2557a0cb88f2ebdb3762
nidhi <kumarinidhi0594@gmail.com>
2025-08-12 15:33:06 +0200

    LocalStorage Added

--- src/app/Store/quizstore.ts
+++ src/app/Store/quizstore.ts
@@ -1,6 +1,6 @@
-import { patchState, signalStore, withComputed, withMethods, withState } from "@ngrx/signals"
-import { initialQuizSlice } from "./quizslice.";
-import { computed } from "@angular/core";
+import { getState, patchState, signalStore, withComputed, withHooks, withMethods, withState } from "@ngrx/signals"
+import { initialQuizSlice, QuizSlice } from "./quizslice.";
+import { computed, effect } from "@angular/core";
 import { addAnswerUpdater, resetQuiz } from "./quiz.updaters";
 import { getCorrectCount } from "./quizhelpers";
 
@@ -8,8 +8,8 @@ export const QuizStore = signalStore(
     { providedIn: 'root' },
     withState(initialQuizSlice),
     withComputed((store) => {
-       const currentQuestionIndex = computed(() => store.answer().length);
-        const isDone =  computed(() => store.answer().length === store.questions().length);
+        const currentQuestionIndex = computed(() => store.answer().length);
+        const isDone = computed(() => store.answer().length === store.questions().length);
         const currentQuestion = computed(() => store.questions()[currentQuestionIndex()]);
         const questionsCount = computed(() => store.questions().length);
         const correctCount = computed(() => getCorrectCount(store.answer(), store.questions()));
@@ -22,8 +22,25 @@ export const QuizStore = signalStore(
         }
     }),
     withMethods(store => ({
-        addAnswer: (index: number) =>  patchState(store, addAnswerUpdater(index)),
+        addAnswer: (index: number) => patchState(store, addAnswerUpdater(index)),
         reset: () => patchState(store, resetQuiz())
+    })),
+    withHooks((store) => ({
+       onInit: () => {
+    if (typeof window !== 'undefined' && window.localStorage) {
+        const stateJson = localStorage.getItem('quiz');
+        if (stateJson) {
+            const state = JSON.parse(stateJson) as QuizSlice;
+            patchState(store, state);
+        }
+
+        effect(() => {
+            const state = getState(store);
+            const stateJson = JSON.stringify(state);
+            localStorage.setItem('quiz', stateJson);
+        });
+    }
+}
     }))
 );
 
